5b8fc39367576d17690ced0f1695dce1d1c5f73c
Alex3236 <45303195+alex3236@users.noreply.github.com>
2024-02-19 22:10:40 +0800

    fix: padding-inline

--- app/docs/[page]/page.tsx
+++ app/docs/[page]/page.tsx
@@ -28,7 +28,7 @@ export default async function App({params}: { params: { page: string } }) {
         <Section className="px-0">
             <Container>
                 <div
-                    className="rounded-lg w-fit p-2 ml-10 text-primary/70 dark:text-primary-dark/70 hover:bg-primary/5 dark:hover:bg-primary-dark/10">
+                    className="rounded-lg w-fit p-2 text-primary/70 dark:text-primary-dark/70 hover:bg-primary/5 dark:hover:bg-primary-dark/10">
                     <Link className="flex items-center" href="/">
                         <ArrowLeftCircleIcon className="w-5 h-5 mr-1"/>返回主页
                     </Link>
